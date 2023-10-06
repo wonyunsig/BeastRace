82e89f6aefd0da393342298a3002ab4e5a4f6e7b
wonyunsig <wonyunsig@gmail.com>
2023-10-06 16:21:30 +0900

    EditLobbySystem

--- Assets/Scripts/RoomItem.cs
+++ Assets/Scripts/RoomItem.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,9 +10,25 @@ public class RoomItem : MonoBehaviour
 {
 
   public TextMeshProUGUI roomName;
+  private LobbyManager manager;
+
+  private void Start()
+  {
+    manager = FindObjectOfType<LobbyManager>();
+  }
 
   public void SetRoomName(string _roomName)
   {
     roomName.text = _roomName;
+  }
+
+  public void OnClickItem()
+  {
+    manager.JoinRoom(roomName.text);
+  }
+
+  public void JoinRoom(string roomName)
+  {
+    PhotonNetwork.JoinRoom(roomName);
   }
 }
